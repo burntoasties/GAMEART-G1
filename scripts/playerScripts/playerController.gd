a0bdc4c20ff1d8211f052ee4d21c5886fc98947b
burntoasties <anton_borromeo@dlsu.edu.ph>
2025-11-15 01:43:28 +0800

    Quick Fix
    Forgot to add a file for init

--- scripts/playerScripts/playerController.gd
+++ scripts/playerScripts/playerController.gd
@@ -2,9 +2,10 @@ extends CharacterBody2D
 
 @export var speed:float = 50.0
 @onready var anim_tree:AnimationTree = get_node("AnimationTree")
+@onready var timer:Timer = get_node("Timer")
 
 func _physics_process(_delta):
-	var input_vec = Input.get_vector("left", "right", "up", "down")
+	var input_vec = Input.get_vector("left", "right", "up", "down") # Defined in engine settings
 	velocity = input_vec * speed
 	
 	if input_vec == Vector2.ZERO:
@@ -16,3 +17,7 @@ func _physics_process(_delta):
 		anim_tree.set("parameters/Attack/BlendSpace2D/blend_position", input_vec)
 	
 	move_and_slide()
+
+## I'm trying to make the blink idle animation work, saving this for now
+func _on_timer_timeout() -> void:
+	pass
